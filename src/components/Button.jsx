0ab504ebf6cf9ea568383b355824778c9551fac2
excell-09 <excell.agcb12@gmail.com>
2023-09-30 15:37:03 +0700

    button text no wrap

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Button({ className, children }) {
   return (
     <button
-      className={`${className} flex items-center justify-center gap-1 font-light text-white bg-blue-500 px-5 py-1 rounded-full`}>
+      className={`${className} flex items-center justify-center gap-1 font-light text-white bg-blue-500 px-5 py-1 rounded-full whitespace-nowrap`}>
       {children}
     </button>
   );
